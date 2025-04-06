eeecc82b655f4ea97545dab87f76c1241fc5fafe
Чеченов Роберт Адамович <rachechenov@edu.hse.ru>
2025-04-06 09:13:56 +0300

    several changes to fix redirecting attempt 3

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,7 +52,7 @@ application.add_middleware(
 # Middleware для CORS
 application.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=security_config.CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
